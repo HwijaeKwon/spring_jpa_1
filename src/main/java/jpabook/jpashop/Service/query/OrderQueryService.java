0c1820e49b5ffb7037743c69fb7d2e825c0b6e78
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-02-21 15:12:19 +0900

    [feat] 주문 서비스 구현
    - 주문 서비스 구현

---
+++ src/main/java/jpabook/jpashop/Service/query/OrderQueryService.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.Service.query;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+}
