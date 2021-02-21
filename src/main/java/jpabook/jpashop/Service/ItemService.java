94fa8e69c1a44e58159781b4557535c9a327ef2e
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-02-21 15:13:25 +0900

    [feat] 상품 서비스, 레포지토리 구현
    - 상품 서비스, 레포지토리 구현

---
+++ src/main/java/jpabook/jpashop/Service/ItemService.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.GeneratedValue;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long saveItem(Item item) {
+        itemRepository.save(item);
+        return item.getId();
+    }
+
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity) {
+        //findItem은 db에서 조회한 item -> 영속성 객체
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        //나머지 필드도 채운다고 가정한다
+        //이렇게 변경만 하면 db에 반영된다 commit -> flush -> 변경 감지
+        return findItem;
+    }
+
+    public Item findItem(Item item) {
+        return itemRepository.findOne(item.getId());
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
