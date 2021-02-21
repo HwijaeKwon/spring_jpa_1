e55aab60a2090bd4bac6537a13e7776813a5258d
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-02-21 15:07:52 +0900

    [feat] Member 서비스 구현
    - Member 서비스 구현

---
+++ src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //조회는 readOnly
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    @Transactional //join은 read only가 아니므로 따로 설정
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 조회
+     * @return
+     */
+    public Member findMember(Member member) {
+        return memberRepository.findById(member.getId()).get();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId).get();
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findById(id).get();
+        member.setName(name); // 변경 감지를 이용한다
+    }
+}
